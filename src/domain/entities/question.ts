21bdfef313bd02c3d466a499d0689a4c7dc2528f
Bruno Fukumori <brunofukumori@gmail.com>
2023-12-20 17:05:53 -0300

    feat: add value object for slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from 'node:crypto';
+import { Slug } from './value-objects/slug';
 
 export class Question {
   constructor(
     public title: string,
+    public slug: Slug,
     public content: string,
     public authorId: string,
     public id?: string
