755256fa90a1ce0bc5dc38fa527271478a1fb717
Bruno Fukumori <brunofukumori@gmail.com>
2023-12-20 15:48:42 -0300

    feat: add relationship between entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,7 +1,12 @@
 import { randomUUID } from 'node:crypto';
 
 export class Answer {
-  constructor(public content: string, public id?: string) {
+  constructor(
+    public content: string,
+    public authorId: string,
+    public questionId: string,
+    public id?: string
+  ) {
     this.id = id ?? randomUUID();
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -4,6 +4,7 @@ export class Question {
   constructor(
     public title: string,
     public content: string,
+    public authorId: string,
     public id?: string
   ) {
     this.id = id ?? randomUUID();
