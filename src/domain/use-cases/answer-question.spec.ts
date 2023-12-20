d092d37e875e97d035c39605498925f80f4720a3
Bruno Fukumori <brunofukumori@gmail.com>
2023-12-20 19:44:09 -0300

    chore: add vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question';
 import { AnswersRepository } from '@domain/repositories/answers-repository';
 import { Answer } from '@domain/entities/answer';
