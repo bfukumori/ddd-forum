f6836fe8c2c7260959398aec09ec6166beb85638
Bruno Fukumori <brunofukumori@gmail.com>
2024-01-02 23:55:09 -0300

    feat: add fetch question answers

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,27 @@
+import { type Answer } from '@domain/forum/enterprise/entities/answer';
+import { type AnswersRepository } from '../repositories/answers-repository';
+
+interface FetchQuestionAnswersUseCaseRequest {
+  page: number;
+  questionId: string;
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[];
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private readonly answersRepository: AnswersRepository) {}
+
+  async execute({
+    page,
+    questionId,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    );
+
+    return { answers };
+  }
+}
